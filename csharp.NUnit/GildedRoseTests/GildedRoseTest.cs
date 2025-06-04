0fad383f7c8a2963e8306b13f3fda60d4214d806
KaltMax <mkaltenr@gmx.at>
2025-06-04 14:58:42 +0200

    Implement GiledRoseTest setup and fixed the existing failing test.

--- csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
+++ csharp.NUnit/GildedRoseTests/GildedRoseTest.cs
@@ -6,12 +6,50 @@ namespace GildedRoseTests;
 
 public class GildedRoseTest
 {
+    private IList<Item> _items;
+    private GildedRose _app;
+
+    [SetUp]
+    public void Setup()
+    {
+        _items = new List<Item>
+        {
+            new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+            new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+            new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+            new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+            new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+            new Item
+            {
+                Name = "Backstage passes to a TAFKAL80ETC concert",
+                SellIn = 15,
+                Quality = 20
+            },
+            new Item
+            {
+                Name = "Backstage passes to a TAFKAL80ETC concert",
+                SellIn = 10,
+                Quality = 49
+            },
+            new Item
+            {
+                Name = "Backstage passes to a TAFKAL80ETC concert",
+                SellIn = 5,
+                Quality = 49
+            },
+            // this conjured item does not work properly yet
+            new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+        };
+
+        _app = new GildedRose(_items);
+    }
+
     [Test]
     public void Foo()
     {
         var items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
         var app = new GildedRose(items);
         app.UpdateQuality();
-        Assert.That(items[0].Name, Is.EqualTo("fixme"));
+        Assert.That(items[0].Name, Is.EqualTo("foo"));
     }
 }
